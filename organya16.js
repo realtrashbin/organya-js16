9bae7eb046586f9be0023d1907b321b3ddeb36e9
Bi_N <122274598+realtrashbin@users.noreply.github.com>
2026-03-13 06:16:08 -0400

    Update organya16.js

--- organya16.js
+++ organya16.js
@@ -19,6 +19,7 @@
             const orgVersion = view.getUint16(p, true); p += 2;
             if (orgVersion != 0x3631) {
                 throw "File is not org16.";
+               return;
             }
 
             this.wait = view.getUint16(p, true); p += 2;
