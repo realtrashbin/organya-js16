4eb758086f1e6adb5028688d1273b74c5892e931
Bi_N <122274598+realtrashbin@users.noreply.github.com>
2026-03-25 16:36:56 -0400

    Update organya-ui.js

--- organya-ui.js
+++ organya-ui.js
@@ -142,7 +142,7 @@
                     if (noteIdx === -1) continue;
 
                     const sprTailX = 32;
-                    const sprTailY = track * 4;
+                    const sprTailY = (track * 4)+16;
 
                     let x = 64;
                     while (x < width) {
